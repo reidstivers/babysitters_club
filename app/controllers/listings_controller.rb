3dc10f3e61f76dc0d65544159d51267f6b454e6a
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-07-16 20:33:26 +0900

    Listings controller create and new method

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,4 +6,21 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def new
+    @listing = Listing.new
+  end
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def listing_params
+    params.require(:listing).permit(:name, :photo)
+  end
 end

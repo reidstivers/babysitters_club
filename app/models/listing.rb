174f6184950aaa6394f4525f07d576720c567ccd
Kurtz <jonathankrtz@gmail.com>
2024-07-16 20:15:30 +0900

    Added location to Users and listings, updated the devise sign up to account for this

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :title, :description, :price, presence: true
+  validates :title, :description, :price, :location, presence: true
   validates :description, length: { minimum: 10 }
 end
---
+++ db/migrate/20240716105944_add_location_tolisting.rb
@@ -0,0 +1,5 @@
+class AddLocationTolisting < ActiveRecord::Migration[7.1]
+  def change
+    add_column :listings, :location, :string
+  end
+end

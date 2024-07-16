92da9bdb1a7e154b52239da2856dabf0c6cddaa9
Kurtz <jonathankrtz@gmail.com>
2024-07-16 19:52:45 +0900

    Added validatations to the models for presence, as well as a minimum length for listing description

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :title, :description, :price, presence: true
+  validates :description, length: { minimum: 10 }
 end

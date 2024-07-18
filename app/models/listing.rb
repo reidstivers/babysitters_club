ee508344c62f4db92f809b50d24c89bd77206a65
Kurtz <jonathankrtz@gmail.com>
2024-07-18 17:35:37 +0900

    photos field added to Listing and the Faker seed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :photos
 
   validates :title, :description, :fee, :location, presence: true
   validates :description, length: { minimum: 10 }

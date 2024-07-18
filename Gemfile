00e5a3ac4d18e01de27d91b4ad3ae16608646844
Kurtz <jonathankrtz@gmail.com>
2024-07-18 20:32:25 +0900

    Got the dates for the bookings validating properly and fixed the redirect for a booking save

--- Gemfile
+++ Gemfile
@@ -78,3 +78,4 @@ end
 # Manually Added Gems
 gem "devise"
 gem "faker"
+gem "validates_timeliness", '~> 7.0.0.beta1'
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,15 +15,15 @@ class BookingsController < ApplicationController
     @booking.listing_id = params[:listing_id]
     @booking.user = current_user
     if @booking.save
-      redirect_to listing_path(params[:listing_id])
-          else
-      render :new, status: :unprocessable_entity
+      redirect_to bookings_path, notice: "Booking created successfully"
+    else
+      redirect_to listing_path(params[:listing_id]), status: :unprocessable_entity
     end
   end
 
 private
 
   def booking_params
-    params.require(:booking).permit(:start_at, :end_at)
+    params.require(:booking).permit(:start_at, :end_at, :notes)
   end
 end

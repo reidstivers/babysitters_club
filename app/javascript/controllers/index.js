e1f7702b15c70e3915cca290626f4b47ff7a7264
Reid Stivers <reidstivers@gmail.com>
2024-07-27 01:15:32 +0900

    have the weird only left carousel but want to transfer to the laptop

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,9 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+import Carousel from '@stimulus-components/carousel'
 
+application.register('carousel', Carousel)
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
@@ -25,3 +25,8 @@ pin "nanoid", to: "https://ga.jspm.io/npm:nanoid@3.3.7/index.browser.js"
 pin "subtag", to: "https://ga.jspm.io/npm:subtag@0.5.0/subtag.js"
 pin "suggestions", to: "https://ga.jspm.io/npm:suggestions@1.7.1/index.js"
 pin "xtend", to: "https://ga.jspm.io/npm:xtend@4.0.2/immutable.js"
+pin "@stimulus-components/carousel", to: "https://ga.jspm.io/npm:@stimulus-components/carousel@6.0.0/dist/stimulus-carousel.mjs"
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@8.4.7/swiper-bundle.esm.js"
+pin "stimulus-carousel", to: "https://ga.jspm.io/npm:stimulus-carousel@5.0.1/dist/stimulus-carousel.mjs"
+pin "dom7", to: "https://ga.jspm.io/npm:dom7@4.0.6/dom7.esm.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
